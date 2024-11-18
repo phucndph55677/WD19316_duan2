3e67837f09b21b397aa9b7ed4147302aae914847
dattr11 <talonin12@gmail.com>
2024-11-19 00:33:13 +0700

    Crub cate

---
+++ Admin/Controller/CateAdminController.php
@@ -0,0 +1,11 @@
+<?
+class CateAdminController
+{
+    public function listCate()
+    {
+        echo "Danh sách danh muc";
+        // $cateAdmin = new CateAdmin();
+        // $cates = $cateAdmin->getCates();
+        // require_once 'Admin/Views/Cate/list.php';
+    }
+}
